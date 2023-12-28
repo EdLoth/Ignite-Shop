55a24b0b3709a1d0fcb3559fb72a8ee11579390f
Eduardo Ramos <eduardosram10@gmail.com>
2023-12-28 17:41:10 -0300

    Challange Completed :tada:

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,23 @@
-import { globalStyles } from '@/styles/global'
-import type { AppProps } from 'next/app'
-import logoImg from '@/assets/logo.svg'
-import Image from 'next/image';
-import { Container, Header } from '@/styles/pages/app';
+import { AppProps } from "next/app"
+import { globalStyles } from "../styles/global"
 
-globalStyles();
-export default function App({ Component, pageProps }: AppProps) {
+import logoImg from "../assets/logo.svg"
+import { Container, Header } from "../styles/pages/app"
+
+import Image from "next/image"
+
+globalStyles()
+
+function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
-    <Header>
-      
+      <Header>
         <Image src={logoImg} alt="" />
-    </Header>
+      </Header>
+
       <Component {...pageProps} />
     </Container>
   )
 }
+
+export default App
